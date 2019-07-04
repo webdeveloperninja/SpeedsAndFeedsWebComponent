50a3c3bac50589200cb064d378065d58ca48de4a
Your Name <rsmith5901@gmail.com>
2019-07-04 10:16:40 -0400

    asfdfsdasafd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { AkitaNgFormsManager } from '@datorama/akita-ng-forms-manager';
+
+export const formName = 'cutData';
+
+export interface FormsState {
+  cutData: {
+    materialToCut: string;
+    toolMaterialType: number;
+    toolDiameter: string;
+    numberOfFlutes: string;
+  };
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'speeds-feeds-calculator';
+export class AppComponent implements OnInit, OnDestroy {
+  readonly formData$ = this.formsManager.selectForm(formName);
+
+  toolForm = this._formBuilder.group({
+    materialToCut: [''],
+    toolMaterialType: [''],
+    toolDiameter: [''],
+    numberOfFlutes: ['']
+  });
+
+  constructor(private readonly _formBuilder: FormBuilder, private formsManager: AkitaNgFormsManager<FormsState>) {}
+
+  ngOnInit() {
+    this.formsManager.upsert(formName, this.toolForm);
+  }
+
+  ngOnDestroy() {
+    this.formsManager.unsubscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Injector } from '@angular/core';
 import { createCustomElement } from '@angular/elements';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, ReactiveFormsModule, environment.production ? [] : AkitaNgDevtools.forRoot()],
   providers: [],
   entryComponents: [AppComponent]
 })
