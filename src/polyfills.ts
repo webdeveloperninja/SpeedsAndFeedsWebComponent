4e56c9a63b527db83ef282a0f6a34df7459eedaa
Your Name <rsmith5901@gmail.com>
2019-07-05 10:12:22 -0400

    no ie

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -77,9 +77,6 @@ import 'zone.js/dist/zone'; // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
-// Used for browsers with partially native support of Custom Elements
-import '@webcomponents/custom-elements/src/native-shim';
 
-// Used for browsers without a native support of Custom Elements
+import '@webcomponents/custom-elements/src/native-shim';
 import '@webcomponents/custom-elements/custom-elements.min';
-import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js';
